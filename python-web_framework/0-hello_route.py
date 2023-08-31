f1cda7e5912fdfd68081227dbaf072c6d7763589
Wuraola-Ola <113630693+Wuraola-Ola@users.noreply.github.com>
2023-08-31 22:04:25 +0100

    python plus html

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Starts a Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!' when accessing root route."""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
